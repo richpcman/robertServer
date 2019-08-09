bd9116a3fa4a9a21325a8f06e4c6681c6d6154a9
eyesight69 <eyesight69@gmail.com>
2019-08-09 12:20:25 +0800

    init1

---
+++ kkk/src/kkk/Main.java
@@ -0,0 +1,60 @@
+package kkk;
+
+public class Main {
+public static void main(String[] args) {
+	System.out.println("<?xml version=\"1.0\" encoding=\"Big5\" ?> " +
+            "<upg_createCashPaymentReq_req>" +
+            "<account_id>" +
+            "account_id"+
+            "</account_id>>" +
+            "<msisdn>" +
+            "msisdn"+
+            "</msisdn>" +
+            "<amount>" +
+            "amount"+
+            "</amount>" +
+            "<payment_source_id>" +
+            "payment_source_id"+
+            "</payment_source_id>" +
+            "<bank_code>" +
+            "bank_code" +
+            "</bank_code>" +
+            "<bank_account>" +
+            "bank_account"+
+            "</bank_account>" +
+            "<card_num>" +
+            "card_num"+
+            "</card_num>" +
+            "<cc_expire>" +
+            "cc_expire" +
+            "</cc_expire>" +
+            "<card_cvv2>" +
+            "card_cvv2"+
+            "</card_cvv2>" +
+            "<deposit_date>" +
+            "deposit_date"+
+            "</deposit_date>" +
+            "<memo_text>" +
+            "memo_text"+
+            "</memo_text>" +
+            "<un_hotline_flag>" +
+            "un_hotline_flag"+
+            "</un_hotline_flag>" +
+            "<opid>" +
+            "opid"+
+            "</opid>" +
+            "<rocid>" +
+            "rocid"+
+            "</rocid>" +
+            "<external_sys_key>" +
+            "external_sys_key"+
+            "</external_sys_key>" +
+            "<external_sys_info1>" +
+            "external_sys_info1"+
+            "</external_sys_info1>" +
+            "<external_sys_info2>" +
+            "external_sys_info2"+
+            "</external_sys_info2>" +
+            "</upg_createCashPaymentReq_req>");
+}
+}
